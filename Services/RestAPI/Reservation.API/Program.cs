937b6b5553e8b00f7af667f536f787c283d1d300
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-01-26 00:37:59 +0300

    Contact.API için Infrastructure, Models ve Services projeleri oluşturuldu, appserttings yapıldı

--- Services/RestAPI/Reservation.API/Program.cs
+++ Services/RestAPI/Reservation.API/Program.cs
@@ -20,6 +20,7 @@ namespace Reservation.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:9000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- Services/RestAPI/Reservation.API/Startup.cs
+++ Services/RestAPI/Reservation.API/Startup.cs
@@ -36,7 +36,7 @@ namespace Reservation.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
